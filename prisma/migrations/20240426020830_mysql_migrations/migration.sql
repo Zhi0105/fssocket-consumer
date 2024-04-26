1f1a9aa620a1dcca0a361a1b532dee26d01816a1
Argie <argie.b@flowerstore.ph>
2024-04-26 14:01:26 +0800

    replace db to mysql

---
+++ prisma/migrations/20240426020830_mysql_migrations/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `logs` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `session` VARCHAR(191) NOT NULL,
+    `data` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `logs_session_key`(`session`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
